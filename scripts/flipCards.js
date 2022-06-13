c31aa9b76111c336a40c6eadc751d14285ef5b6e
Seth Lamancusa <sethlamancusa@gmail.com>
2022-06-13 13:28:25 -0400

    progress

--- scripts/flipCards.js
+++ scripts/flipCards.js
@@ -3,7 +3,6 @@ let state = [];
 flip = (id) => {
     const inner = document.querySelector("#card_" + id + ">*");
 
-    console.log(state);
     if (state[id] != 1) {
         inner.style.transform = `rotateY(180deg)`;
         state[id] = 1;
