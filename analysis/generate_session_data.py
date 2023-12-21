1788e21c828ee60736756042b37c036ed8f9e4c8
Seth Lamancusa <sethlamancusa@gmail.com>
2023-12-20 19:13:19 -0500

    Revert "Linear models"
    This reverts commit 55944c9564eb03ce0be8317e4c076a0c6ca9bc1a.

--- analysis/generate_session_data.py
+++ analysis/generate_session_data.py
@@ -2,7 +2,7 @@ from generate_plot import generate_plot
 from generate_text_summary import generate_text_summary
 from localStorage_to_raw import localStorage_to_raw
 from raw_to_session import raw_to_session
-from generate_lms import generate_lms
+from generate_lm import generate_lm
 
 
 # Define the relative path to the input json file
@@ -12,5 +12,5 @@ output_folder = "analysis/data/session_1"
 raw_path = localStorage_to_raw(input_file_path, output_folder)
 session_path = raw_to_session(raw_path, output_folder)
 summary_path = generate_text_summary(session_path, output_folder)
-generate_lms(session_path, output_folder)
+generate_lm(session_path, output_folder)
 generate_plot(session_path, output_folder)
