d2bf046ad436f9a2c27c61eb0fbd3eef6159e068
Seth Lamancusa <sethlamancusa@gmail.com>
2023-12-21 20:34:47 -0500

    Rolling averages

--- analysis/generate_session_data.py
+++ analysis/generate_session_data.py
@@ -3,6 +3,7 @@ from generate_text_summary import generate_text_summary
 from localStorage_to_raw import localStorage_to_raw
 from raw_to_session import raw_to_session
 from generate_lm import generate_lm
+from generate_rolling import generate_rolling
 
 
 # Define the relative path to the input json file
@@ -12,6 +13,8 @@ output_folder = "analysis/data/session_1"
 raw_path = localStorage_to_raw(input_file_path, output_folder)
 session_path = raw_to_session(raw_path, output_folder)
 summary_path = generate_text_summary(session_path, output_folder)
+for i in range(10, 51, 10):
+    generate_rolling(session_path, output_folder, i)
 generate_lm(
     session_path,
     output_folder,
