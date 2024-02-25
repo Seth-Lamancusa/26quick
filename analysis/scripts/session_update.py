7b58ccaf11f739a41d9c6b17e17bfaa2acb6894d
slamancusa <sethlamancusa@gmail.com>
2024-02-25 17:59:21 -0500

    44

--- analysis/scripts/session_update.py
+++ analysis/scripts/session_update.py
@@ -5,45 +5,45 @@ from raw_to_session import raw_to_session
 from generate_lm_folder import generate_lm_folder
 from generate_rolling import generate_rolling
 
-session_ID = "43"
+session_ID = "44"
 sessions = [str(i) for i in range(1, int(session_ID) + 1)]
 
-# # Preprocessing
-# localStorage_to_raw(session_ID)
-# raw_to_session(session_ID)
+# Preprocessing
+localStorage_to_raw(session_ID)
+raw_to_session(session_ID)
 
-# # Generate session insights
-# generate_text_summary(session_ID=session_ID)
-# for i in range(10, 51, 10):
-#     generate_rolling(session_ID, i)
-# generate_lm_folder(
-#     predictor="Total Mistakes",
-#     response="Total Time Taken (ms)",
-#     session_ID=session_ID,
-#     output_folder_name="time_given_mistakes",
-# )
-# generate_lm_folder(
-#     predictor="Layout Difficulty",
-#     response="Total Time Taken (ms)",
-#     session_ID=session_ID,
-#     output_folder_name="time_given_diff",
-# )
-# generate_plots(session_ID=session_ID)
+# Generate session insights
+generate_text_summary(session_ID=session_ID)
+for i in range(10, 51, 10):
+    generate_rolling(session_ID, i)
+generate_lm_folder(
+    predictor="Total Mistakes",
+    response="Total Time Taken (ms)",
+    session_ID=session_ID,
+    output_folder_name="time_given_mistakes",
+)
+generate_lm_folder(
+    predictor="Layout Difficulty",
+    response="Total Time Taken (ms)",
+    session_ID=session_ID,
+    output_folder_name="time_given_diff",
+)
+generate_plots(session_ID=session_ID)
 
-# # Update aggregate insights
-# generate_text_summary(agg=True, sessions=sessions)
-# generate_lm_folder(
-#     predictor="Total Mistakes",
-#     response="Total Time Taken (ms)",
-#     output_folder_name="time_given_mistakes",
-#     agg=True,
-#     sessions=sessions,
-# )
-# generate_lm_folder(
-#     predictor="Layout Difficulty",
-#     response="Total Time Taken (ms)",
-#     output_folder_name="time_given_diff",
-#     agg=True,
-#     sessions=sessions,
-# )
+# Update aggregate insights
+generate_text_summary(agg=True, sessions=sessions)
+generate_lm_folder(
+    predictor="Total Mistakes",
+    response="Total Time Taken (ms)",
+    output_folder_name="time_given_mistakes",
+    agg=True,
+    sessions=sessions,
+)
+generate_lm_folder(
+    predictor="Layout Difficulty",
+    response="Total Time Taken (ms)",
+    output_folder_name="time_given_diff",
+    agg=True,
+    sessions=sessions,
+)
 generate_plots(agg=True, sessions=sessions)
